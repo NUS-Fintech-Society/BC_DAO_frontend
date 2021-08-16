a45f4412fb21d240bd3653b5f9a150d599d34a56
skcheongbrian <skcheongbrian@gmail.com>
2021-08-16 23:30:29 +0800

    Added Header and BlockChainDescription components for the landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Header from "../components/LandingPage/Header"
+import BlockChainDescription from "../components/LandingPage/BlockChainDescription";
 
 export default function index() {
   return (
     <div>
       <NavBar />
-      test
+      <Header />
+      <BlockChainDescription />
     </div>
   );
 }
