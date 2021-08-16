170cdcf79417877fb9d096d0f989545bca0956b4
ong6 <junxiongong2@gmail.com>
2021-08-16 21:31:22 +0800

    NavBar routing fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./pages";
-import Vote from "./pages/vote.jsx";
+import Vote from "./pages/vote";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route path="/" component={Home} />
           <Route path="/vote" component={Vote} />
+          <Route path="/" component={Home} />
+          <Redirect to="/" />
         </Switch>
       </Router>
     </div>
