42c2d21dd68c25fb2ff572bf7f5cb023aa81d2a4
Junx <junxiongong2@gmail.com>
2021-08-17 23:32:40 +0800

    Update routing

--- src/components/Vote/VoteList.jsx
+++ src/components/Vote/VoteList.jsx
@@ -15,20 +15,10 @@ const vote_list = [sample_content, sample_content];
 
 export default function VoteList() {
   return (
-    <div className="flex flex-col max-w-7xl mx-auto p-2">
-      <div className="flex flex-row items-center justify-between px-4 ">
-        <div className="text-4xl text-gray-700 my-4">Proposals</div>
-        <a href="/vote/new-proposal">
-          <div className="bg-gray-200 text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
-            New Proposal
-          </div>
-        </a>
-      </div>
-      <div className="flex flex-col p-2 space-y-4">
-        {vote_list.map((vote) => (
-          <VoteListItem content={vote} key={vote.id} />
-        ))}
-      </div>
+    <div className="flex flex-col p-2 space-y-4">
+      {vote_list.map((vote) => (
+        <VoteListItem content={vote} key={vote.id} />
+      ))}
     </div>
   );
 }
--- src/pages/vote/index.jsx
+++ src/pages/vote/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Link, useRouteMatch, Switch, Route } from "react-router-dom";
 import NavBar from "../../components/Layout/NavBar";
 import VoteList from "../../components/Vote/VoteList";
 
-export default function vote() {
+export default function Vote() {
+  let { path, url } = useRouteMatch();
+
   return (
     <div className="w-full h-full">
       <NavBar />
-      <VoteList />
+      <div className="flex flex-col max-w-7xl mx-auto p-2">
+        <Switch>
+          <Route exact path={path}>
+            <div className="flex flex-row items-center justify-between px-4 ">
+              <div className="text-4xl text-gray-700 my-4">Proposals</div>
+              <Link to={`${url}/new-proposal`}>
+                <div className="bg-gray-200 text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                  New Proposal
+                </div>
+              </Link>
+            </div>
+            <VoteList />
+          </Route>
+          <Route path={`${path}/:id`}>to the place</Route>
+        </Switch>
+      </div>
     </div>
   );
 }
