cdc2cb3e191170a046a6050f8c12ee075c4b3fb9
ong6 <junxiongong2@gmail.com>
2021-09-08 02:07:51 +0800

    NavBar Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -7,29 +7,10 @@ import {
 } from "react-router-dom";
 import Home from "./pages";
 import Vote from "./pages/vote";
-import { useWeb3 } from "@openzeppelin/network/lib/react";
-import { getContract, getProposalHashes } from "./components/api/Api";
-const { projectId } = require("./secrets.json");
 
 function App() {
-  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${projectId}`);
-  const { lib: web3, networkId, accounts, providerName } = web3Context;
-
-  const requestAuth = (web3Context) => web3Context.requestAuth();
-  const requestAccess = useCallback(() => requestAuth(web3Context), []);
-
   return (
     <div>
-      {/* move to nav */}
-      {accounts && accounts.length ? (
-        <div>
-          <div>{networkId === 3 ? accounts[0] : "Get on Ropsten Testnet"}</div>
-        </div>
-      ) : !!networkId && providerName !== "infura" ? (
-        <button onClick={requestAccess}>Login</button>
-      ) : (
-        <div>Please install Ethereum Compatible wallet</div>
-      )}
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
